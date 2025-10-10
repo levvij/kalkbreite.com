5e7b8f64f118c58af20b0da1d6f4504b6ca7bfba
rahelv <rahelkempf@gmx.ch>
2025-10-10 20:32:36 +0200

    WIP Add railcar capture flipping

--- site/page/shared/boxed.ts
+++ site/page/shared/boxed.ts
@@ -1,7 +1,9 @@
 import { display, marginInline, maxWidth, rem } from "@acryps/style";
 
+export const maximumBoxedWidth = rem(80);
+
 export const boxed = () => [
 	display('block'),
-	maxWidth(rem(80)),
+	maxWidth(maximumBoxedWidth),
 	marginInline('auto'),
 ];
