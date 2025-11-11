b3f0526e8f00051a32f891e1d87a9d20d89262be
Levi Hechenberger Di Valentino <levi.hechenberger@acryps.com>
2025-11-11 17:39:20 +0100

    Add vertical train list

--- site/page/trains/train/index.style.ts
+++ site/page/trains/train/index.style.ts
@@ -1,4 +1,4 @@
-import { alignItems, background, backgroundColor, backgroundImage, border, borderColor, boxShadow, child, color, colorStop, display, flexDirection, flexWrap, fontFamily, fontSize, fontWeight, gap, height, hover, justifyContent, linearGradient, marginBottom, marginInline, marginRight, marginTop, overflowInline, overflowX, padding, paddingBlock, paddingInline, percentage, position, px, rem, turn } from "@acryps/style";
+import { alignItems, aspectRatio, background, backgroundColor, backgroundImage, border, borderColor, boxShadow, child, color, colorStop, display, flexDirection, flexGrow, flexWrap, fontFamily, fontSize, fontWeight, gap, height, hover, justifyContent, left, linearGradient, marginBlock, marginBottom, marginInline, marginLeft, marginRight, marginTop, overflowInline, overflowX, padding, paddingBlock, paddingInline, percentage, position, px, Ratio, rem, rotate, top, transform, transformOrigin, turn, Variable, width, zIndex } from "@acryps/style";
 import { boxed } from "../../shared/boxed";
 import { captureBackgroundColor, pageColor, pageContrastColor, pageGutter, pageSpacing, primaryColor, runningNumberFont, tagFont } from "../../index.style";
 import { trainIdentifierFont } from "../../assets/font";
@@ -12,7 +12,9 @@ import { assignTrainLabelStyle } from "./label/index.style";
 
 const iconButtonTextSize = rem(1);
 const iconButtonSize = iconButtonTextSize.add(pageGutter.multiply(2));
-const imageHeight = rem(8);
+const imageWidth = rem(8);
+
+export const imageRatio = new Variable<Number>('image-ratio');
 
 export const trainStyle = () => child('ui-train')(
 	display('block'),
@@ -49,22 +51,27 @@ export const trainStyle = () => child('ui-train')(
 	),
 
 	child('ui-units') (
-		display('flex'),
-		alignItems('flex-start'),
-		marginInline(pageSpacing.invert()),
-		marginBottom(pageSpacing),
-		paddingInline(iconButtonSize.divide(2).add(pageSpacing)),
+		display('block'),
 
-		overflowX('scroll'),
+		marginBlock(pageSpacing.add(iconButtonSize.divide(2))),
 
 		child('ui-unit') (
 			display('flex'),
-			flexDirection('column'),
+			height(imageWidth.multiply(imageRatio)),
+			position('relative'),
 
 			clickable(),
 
 			child('img') (
-				height(imageHeight),
+				position('absolute'),
+				top(0),
+				left(imageWidth),
+
+				height(imageWidth),
+
+				transform(rotate(turn(0.25))),
+				transformOrigin('left', 'top'),
+
 				backgroundColor(captureBackgroundColor),
 				backgroundImage(
 					linearGradient(turn(0.125),
@@ -83,8 +90,11 @@ export const trainStyle = () => child('ui-train')(
 			),
 
 			child('ui-detail') (
+				flexGrow(1),
+
 				display('block'),
 				padding(pageGutter),
+				marginLeft(imageWidth.add(pageSpacing.subtract(pageGutter))),
 
 				detailSectionStyle(
 					child('ui-header') (
@@ -103,15 +113,20 @@ export const trainStyle = () => child('ui-train')(
 
 		child('ui-action') (
 			display('flex'),
-			marginInline(iconButtonSize.divide(2).invert()),
-			marginTop(imageHeight.divide(2).subtract(iconButtonSize.divide(2))),
+			marginBlock(iconButtonSize.divide(2).invert()),
+			marginLeft(imageWidth.divide(2).subtract(iconButtonSize.divide(2))),
+			width(iconButtonSize.subtract(pageGutter.multiply(2))),
 			padding(pageGutter.subtract(px(1))),
+
 			position('relative'),
+			zIndex(2),
 
 			clickable(),
 			fontSize(iconButtonTextSize),
 			backgroundColor(pageColor),
 			border(px(1), 'solid', pageContrastColor),
+
+			transform(rotate(turn(0.25))),
 
 			hover() (
 				color(pageColor),
--- site/page/trains/train/index.tsx
+++ site/page/trains/train/index.tsx
@@ -10,24 +10,24 @@ import { markerColor } from "../../index.style";
 import { LayoutLoader } from "../../shared/layout/loader";
 import { Section, SectionPosition } from "@packtrack/layout";
 import { TrainLabelComponent } from "../../shared/train-label";
+import { imageRatio } from "./index.style";
+import { ratio } from "@acryps/style";
 
 export class TrainPage extends Component {
 	declare parameters: { identifier };
 	declare parent: TrainsPage;
 
 	train: TrainViewModel;
-	state: TrainStateViewModel;
 	railcars: TrainRailcarUnitViewModel[];
 	label: TrainLabelViewModel;
 
 	async onload() {
 		this.train = this.parent.trains.find(train => train.identifier == this.parameters.identifier);
-		this.state = await new TrainService().getTrain(this.parameters.identifier);
 		this.railcars = await new TrainService().getTrainRailcars(this.parameters.identifier);
 		this.label = await new TrainService().getLabel(this.parameters.identifier);
 	}
 
-	breadcrumb = () => `Train${this.state.label ? ` ${this.state.label.label}` : ''} #${this.parameters.identifier}`;
+	breadcrumb = () => `Train${this.label?.label ? ` ${this.label.label}` : ''} #${this.parameters.identifier}`;
 	render(child) {
 		if (child) {
 			return <ui-train>
@@ -62,23 +62,7 @@ export class TrainPage extends Component {
 				</ui-action>}
 
 				{this.railcars.map((railcar, index) => [
-					<ui-unit ui-href={`/railcar/${railcar.tag}`}>
-						<img src={`/capture/train/railcar/${railcar.id}/forward`} />
-
-						<ui-detail>
-							{new DetailSectionComponent(<ui-header>
-								<ui-tag>
-									{railcar.tag}
-								</ui-tag>
-
-								<ui-name>
-									{railcar.givenName || railcar.model?.name || railcar.runningNumber}
-								</ui-name>
-							</ui-header>)
-								.addMetric('Type', () => railcar.model?.name, `/model/${railcar.model?.tag}`)
-								.addMetric('Running Number', () => railcar.runningNumber)}
-						</ui-detail>
-					</ui-unit>,
+					this.renderRailcar(railcar),
 
 					Application.session.account && index != this.railcars.length - 1 && <ui-action ui-click={async () => {
 						await new TrainService().uncoupleAfter(railcar.id);
@@ -93,37 +77,37 @@ export class TrainPage extends Component {
 					{coupleIcon()}
 				</ui-action>}
 			</ui-units>
-
-			{this.state.lastHeadPosition && this.renderLayout()}
 		</ui-train>
 	}
 
-	private renderLayout() {
-		const layout = new LayoutComponent();
+	renderRailcar(railcar: RailcarSummaryModel) {
+		const image: HTMLImageElement = <img src={`/capture/train/railcar/${railcar.id}/forward`} />;
 
-		LayoutLoader.load().then(() => requestAnimationFrame(() => {
-			let section: Section;
+		image.onload = () => {
+			element.style.setProperty(
+				imageRatio.propertyName,
+				(image.naturalWidth / image.naturalHeight).toString()
+			);
+		};
 
-			for (let district of layout.layout.allDistricts) {
-				for (let peer of district.sections) {
-					if (peer.domainName == this.state.lastHeadPosition.section) {
-						section = peer;
-					}
-				}
-			}
+		const element: HTMLElement = <ui-unit ui-href={`/railcar/${railcar.tag}`}>
+			{image}
 
-			if (!section) {
-				return;
-			}
+			<ui-detail>
+				{new DetailSectionComponent(<ui-header>
+					<ui-tag>
+						{railcar.tag}
+					</ui-tag>
 
-			layout.highlight(section);
+					<ui-name>
+						{railcar.givenName || railcar.model?.name || railcar.runningNumber}
+					</ui-name>
+				</ui-header>)
+					.addMetric('Type', () => railcar.model?.name, `/model/${railcar.model?.tag}`)
+					.addMetric('Running Number', () => railcar.runningNumber)}
+			</ui-detail>
+		</ui-unit>;
 
-			const head = new SectionPosition(section, this.state.lastHeadPosition.offset, this.state.lastHeadPosition.reversed);
-			const tail = head.advance(-this.train.coupledLength);
-
-			layout.mark(markerColor, head, tail);
-		}));
-
-		return layout;
+		return element;
 	}
 }
